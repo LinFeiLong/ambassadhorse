0a7fdfd4f88685bccee0bbc78ff475ba1c7d380d
elena-75 <contact.pro.elena@gmail.com>
2022-12-01 18:36:04 +0100

    add styling global stylesheet

---
+++ app/theme/styling.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+
+export const styling = StyleSheet.create({
+    CENTER: {
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    ROW: { flexDirection: "row" },
+    ROW_CENTER_Y: {
+        alignItems: "center",
+        flexDirection: "row"
+    }
+})
+
