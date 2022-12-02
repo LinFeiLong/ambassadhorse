5b23bda20b5ffda945ebdbea481d3502f8c42a55
elena-75 <contact.pro.elena@gmail.com>
2022-12-02 10:40:17 +0100

    styling: add flexboxes styles

--- app/theme/styling.ts
+++ app/theme/styling.ts
@@ -5,10 +5,30 @@ export const styling = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center"
     },
+    CENTER_X: {
+        alignItems: "center"
+    },
+    POS_END: {
+        justifyContent: "flex-end"
+    },
+    POS_START: {
+        justifyContent: "flex-start"
+    },
     ROW: { flexDirection: "row" },
+    ROW_CENTER_X: {
+        flexDirection: "row",
+        justifyContent: "center",
+    },
     ROW_CENTER_Y: {
         alignItems: "center",
         flexDirection: "row"
+    },
+    ROW_WRAP: {
+        flexDirection: "row",
+        flexWrap: "wrap"
+    },
+    SPACE_BETWEEN: {
+        justifyContent: "space-between"
     }
 })
 
