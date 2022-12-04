be5fa9e927243c5220f8a774146bdd3c5e73f7c7
elena-75 <contact.pro.elena@gmail.com>
2022-12-04 07:33:19 +0100

    screen: horse -> tmp DATA update + tabs active state added

--- app/screens/HorsesScreen.tsx
+++ app/screens/HorsesScreen.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { FlatList, Image, ImageStyle, TextStyle, TouchableOpacity, View, ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
@@ -12,54 +12,69 @@ const DATA = [
   {
     id: 1,
     title: "Lucky Crypto",
-    subtitle: "1000 parts de 1% à 30€",
-    picture: require('../../assets/images/horse1.png')
+    price: "30 000€",
+    tokenPrice: "30€",
+    picture: require('../../assets/images/horse1.png'),
+    end: "Ends in 30 days"
   },
   {
     id: 2,
     title: "Millenium",
-    subtitle: "1000 parts de 1% à 40€",
-    picture: require('../../assets/images/horse2.png')
+    price: "40 000€",
+    tokenPrice: "40€",
+    picture: require('../../assets/images/horse2.png'),
+    end: "Ends in 4 days"
   },
   {
     id: 3,
     title: "Alyra du web",
-    subtitle: "1000 parts de 1% à 25€",
-    picture: require('../../assets/images/horse3.jpg')
+    price: "20 000€",
+    tokenPrice: "20€",
+    picture: require('../../assets/images/horse3.jpg'),
+    end: "Ends in 15 days"
   },
   {
     id: 4,
     title: "Klodette de Massa",
-    subtitle: "1000 parts de 1% à 60€",
-    picture: require('../../assets/images/horse4.png')
+    price: "35 000€",
+    tokenPrice: "35€",
+    picture: require('../../assets/images/horse4.png'),
+    end: "Ends in 1 hour"
   },
   {
     id: 5,
     title: "Klodette de Massa",
-    subtitle: "1000 parts de 1% à 60€",
-    picture: require('../../assets/images/horse3.jpg')
+    price: "27 000€",
+    tokenPrice: "27€",
+    picture: require('../../assets/images/horse3.jpg'),
+    end: ""
   },
   {
     id: 6,
     title: "Klodette de Massa",
-    subtitle: "1000 parts de 1% à 60€",
-    picture: require('../../assets/images/horse3.jpg')
+    price: "27 000€",
+    tokenPrice: "27€",
+    picture: require('../../assets/images/horse3.jpg'),
+    end: ""
   },
   {
     id: 7,
     title: "Klodette de Massa",
-    subtitle: "1000 parts de 1% à 60€",
-    picture: require('../../assets/images/horse3.jpg')
+    price: "27 000€",
+    tokenPrice: "27€",
+    picture: require('../../assets/images/horse3.jpg'),
+    end: ""
   },
   {
     id: 8,
     title: "Klodette de Massa",
-    subtitle: "1000 parts de 1% à 60€",
-    picture: require('../../assets/images/horse3.jpg')
+    price: "27 000€",
+    tokenPrice: "27€",
+    picture: require('../../assets/images/horse3.jpg'),
+    end: ""
   }
-
-
 ]
+
 
 // REMOVE ME! ⬇️ This TS ignore will not be necessary after you've added the correct navigator param type
 // @ts-ignore
@@ -70,45 +85,46 @@ export const HorsesScreen: FC<StackScreenProps<AppStackScreenProps, "Horses">> =
   // Pull in navigation via hook
   // const navigation = useNavigation()
 
-const Item = ({ title, subtitle, picture }) => {
-  return (
-    <View style={LIST_ITEM_CONTAINER}>
-      <Image style={LIST_IMG} source={picture} />
-      <Text style={LIST_TITLE}>{title}</Text>
-      <Text style={LIST_SUBTITLE}>{subtitle}</Text>
-    </View>
-  )
-}
+  // Tabs
+  const tab = ["Nos 2 ans", "Nos 3 ans", "Nos 4 ans"]
+  const [active, setActive] = useState("Nos 2 ans")
+  const handlePress = (tab) => setActive(tab)
+
+  const Item = ({ picture, title, price, tokenPrice, end  }) => {
+    return (
+      <View style={LIST_ITEM_CONTAINER}>
+        <Text style={LIST_CAPTION}>{end}</Text>
+        <Image style={LIST_IMG} source={picture} />
+        <Text style={LIST_TITLE}>{title}</Text>
+        <Text style={LIST_SUBTITLE}>Total price {price}</Text>
+        <Text style={LIST_SUBTITLE}>Token start price {tokenPrice}</Text>
+      </View>
+    )
+  }
 
   const renderItem = ({ item }) => (
-    <Item title={item.title} subtitle={item.subtitle} picture={item.picture} />
+    <Item picture={item.picture} title={item.title} price={item.price} tokenPrice={item.tokenPrice} end={item.end}  />
   )
-
 
   return (
     <Screen style={$root} preset="scroll">
       <View style={SUBNAV}>
-        <TouchableOpacity>
-          <Text style={[SUBNAV_MENU_ITEM, SUBNAV_MENU_ACTIVE]}>Nos 2 ans</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={SUBNAV_MENU_ITEM}>Nos 3 ans</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={SUBNAV_MENU_ITEM}>Nos 4 ans</Text>
-        </TouchableOpacity>
+        {
+          tab.map(i =>
+            <TouchableOpacity key={i} onPress={() => handlePress(i)} style={(active === i) ? SUBNAV_MENU_ACTIVE : null }>
+              <Text style={SUBNAV_MENU_ITEM}>{i}</Text>
+            </TouchableOpacity>
+          )
+        }
       </View>
 
-      <View>
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-          numColumns={4}
-          columnWrapperStyle={[styling.ROW_CENTER_X,  { alignSelf: "center", width: "99%"   } ]}
-        />
-      </View>
-
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        numColumns={4}
+        columnWrapperStyle={FLATLIST}
+      />
     </Screen>
   )
 })
@@ -138,27 +154,24 @@ const SUBNAV_MENU_ACTIVE: TextStyle = {
 }
 
 // LIST
-const LIST_TITLE: TextStyle = {
-  // TODO : Open Sauce
-  fontSize: 12,
-  paddingTop: 10,
-  color: "white",
-}
-
-const LIST_SUBTITLE: TextStyle = {
-  // TODO : Open Sauce
-  fontFamily: fonts.nunito.bold,
-  fontSize: 14,
-  paddingTop: 9,
-  color: palette.orange
-}
-
 const LIST_ITEM_CONTAINER: ViewStyle = {
+  position: "relative",
+  zIndex: 1,
   width: 310,
   maxWidth: "22%",
   ...styling.POS_END,
   alignSelf: "flex-end",
   margin: 10,
+}
+
+const LIST_CAPTION: TextStyle = {
+  position: "absolute",
+  zIndex: 10,
+  top: 5,
+  left: 5,
+  fontFamily: fonts.nunito.bold,
+  fontSize: 13,
+  color: "black",
 }
 
 const LIST_IMG: ImageStyle = {
@@ -169,4 +182,22 @@ const LIST_IMG: ImageStyle = {
   resizeMode: "cover",
 }
 
+const LIST_TITLE: TextStyle = {
+  fontFamily: fonts.nunito.light,
+  fontSize: 12,
+  paddingVertical: 10,
+  color: "white",
+}
+
+const LIST_SUBTITLE: TextStyle = {
+  fontFamily: fonts.nunito.bold,
+  fontSize: 14,
+  color: palette.orange
+}
+
+const FLATLIST: ViewStyle = {
+  ...styling.ROW_CENTER_X,
+  alignSelf: "center",
+  width: "99%"
+}
 
