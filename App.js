2728fb025ce0ba406505a4cfa6ca2b8d935c10e1
Fernand Lime <fernand.lime@gmail.com>
2022-12-07 02:28:07 +0100

    chore(deps): Add expo-dev-client

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 // This is the entry point if you run `yarn expo:start`
 // If you run `yarn ios` or `yarn android`, it'll use ./index.js instead.
+import "expo-dev-client"
 import App from "./app/app.tsx"
 import React from "react"
 import { registerRootComponent } from "expo"
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,33 +1,36 @@
-import React, { FC, useState } from "react"
-import { observer } from "mobx-react-lite"
-import { TouchableOpacity, View, ViewStyle, Text, TextStyle,Image, ImageStyle } from "react-native"
-import { StackScreenProps } from "@react-navigation/stack"
-import { AppStackScreenProps } from "../navigators"
-import { Btn, Screen } from "../components"
-import { fonts, styling, spacing, gradients, colors } from "../theme"
-import { useNavigation } from "@react-navigation/native"
+import { observer } from 'mobx-react-lite'
+import React, { FC, useState } from 'react'
+import { Image, ImageStyle, Text, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native'
+
+import { useNavigation } from '@react-navigation/native'
+import { StackScreenProps } from '@react-navigation/stack'
+
+import { Btn, Screen } from '../components'
+import { AppStackScreenProps } from '../navigators'
+import { colors, fonts, gradients, spacing, styling } from '../theme'
+
 // import { useStores } from "../models"
-import { StrokedText } from 'stroked-text'
+// import { StrokedText } from 'stroked-text'
 
 // REMOVE ME! ⬇️ This TS ignore will not be necessary after you've added the correct navigator param type
 // @ts-ignore
-export const HomeScreen: FC<StackScreenProps<AppStackScreenProps, "Home">> = observer(function HomeScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+export const HomeScreen: FC<StackScreenProps<AppStackScreenProps, "Home">> = observer(
+  function HomeScreen() {
+    // Pull in one of our MST stores
+    // const { someStore, anotherStore } = useStores()
 
-  // Pull in navigation via hook
-  const navigation = useNavigation()
-  const goToHorses = () => navigation.navigate("Horses")
+    // Pull in navigation via hook
+    const navigation = useNavigation()
+    const goToHorses = () => navigation.navigate("Horses")
 
-  // Subnav
-  const subnav = ["Highlights", "Specifications", "Compare"]
-  const [active, setActive] = useState("Highlights")
-  const handlePress = (subnav) => setActive(subnav)
+    // Subnav
+    const subnav = ["Highlights", "Specifications", "Compare"]
+    const [active, setActive] = useState("Highlights")
+    const handlePress = (subnav) => setActive(subnav)
 
-  return (
-    <Screen style={$root} preset="scroll">
-
-      {/* <View style={SUBNAV}>
+    return (
+      <Screen style={$root} preset="scroll">
+        {/* <View style={SUBNAV}>
         {
           subnav.map(i =>
             <TouchableOpacity key={i} onPress={() => handlePress(i)} style={(active === i) ? SUBNAV_MENU_ACTIVE : null }>
@@ -37,41 +40,51 @@ export const HomeScreen: FC<StackScreenProps<AppStackScreenProps, "Home">> = obs
         }
       </View> */}
 
-      <View style={[styling.ROW, styling.SPACE_BETWEEN]}>
-        <View style={HERO_WRAPPER}>
-          <View style={HERO_CTA_CONTAINER}>
-            <View style={TITLE_CONTAINER}>
-              <Text style={TITLE}>Ambassad'</Text>
-              <StrokedText fill="transparent" stroke="white" strokeWidth={1} style={TITLE_STROKE}>
-                Horse
-              </StrokedText>
-            </View>
-            <Text style={HERO_TEXT}>Vivez {"\n"}l'expérience,{"\n"}devenez{"\n"}propriétaire d'un cheval de sport</Text>
+        <View style={[styling.ROW, styling.SPACE_BETWEEN]}>
+          <View style={HERO_WRAPPER}>
+            <View style={HERO_CTA_CONTAINER}>
+              <View style={TITLE_CONTAINER}>
+                <Text style={TITLE}>Ambassad'</Text>
+                <Text style={TITLE}>Horse</Text>
 
-            <View style={styling.ROW_CENTER_Y}>
-              <Btn text="Investir" textStyle={BTN_GRADIENT_TEXT} gradient={gradients.default} gradientStyle={BTN_GRADIENT} onPress={goToHorses}/>
-              <View style={HERO_SEPARATOR}></View>
-              <Text style={HERO_PRICING}>Commence à 25€</Text>
+                {/* <StrokedText fill="transparent" stroke="white" strokeWidth={1} style={TITLE_STROKE}>
+                Horse
+              </StrokedText> */}
+              </View>
+              <Text style={HERO_TEXT}>
+                Vivez {"\n"}l'expérience,{"\n"}devenez{"\n"}propriétaire d'un cheval de sport
+              </Text>
+
+              <View style={styling.ROW_CENTER_Y}>
+                <Btn
+                  text="Investir"
+                  textStyle={BTN_GRADIENT_TEXT}
+                  gradient={gradients.default}
+                  gradientStyle={BTN_GRADIENT}
+                  onPress={goToHorses}
+                />
+                <View style={HERO_SEPARATOR}></View>
+                <Text style={HERO_PRICING}>Commence à 25€</Text>
+              </View>
             </View>
           </View>
+
+          <Image source={require("../../assets/images/hero.png")} style={HERO_IMG} />
         </View>
-
-        <Image source={ require('../../assets/images/hero.png')} style={HERO_IMG} />
-      </View>
-
-    </Screen>
-  )
-})
+      </Screen>
+    )
+  },
+)
 
 const $root: ViewStyle = {
   flex: 1,
-  backgroundColor: colors.screenBackground
+  backgroundColor: colors.screenBackground,
 }
 
 // SUBNAV
 const SUBNAV: ViewStyle = {
   ...styling.ROW,
-  paddingHorizontal : spacing.screen,
+  paddingHorizontal: spacing.screen,
   paddingTop: 20,
 }
 const SUBNAV_MENU_ITEM: TextStyle = {
@@ -83,7 +96,7 @@ const SUBNAV_MENU_ITEM: TextStyle = {
 }
 const SUBNAV_MENU_ACTIVE: TextStyle = {
   borderBottomColor: "#424242",
-  borderBottomWidth: 3
+  borderBottomWidth: 3,
 }
 const TITLE_CONTAINER: TextStyle = {
   ...styling.ROW_CENTER_Y,
@@ -96,7 +109,7 @@ const TITLE: TextStyle = {
   textTransform: "uppercase",
   color: "white",
 }
-const TITLE_STROKE : TextStyle =  {
+const TITLE_STROKE: TextStyle = {
   fontSize: 29,
   fontFamily: fonts.anton.regular,
   paddingTop: 1, // stroke hack
@@ -108,40 +121,40 @@ const HERO_WRAPPER: ViewStyle = {
 const HERO_CTA_CONTAINER: ViewStyle = {
   minWidth: "40%",
   maxWidth: 450,
-  paddingLeft : spacing.screen,
+  paddingLeft: spacing.screen,
 }
 const HERO_TEXT: TextStyle = {
   fontSize: 41,
   fontFamily: fonts.nunito.light,
-  paddingBottom : 30,
-  color: "white"
+  paddingBottom: 30,
+  color: "white",
 }
 const BTN_GRADIENT: ViewStyle = {
   ...styling.CENTER,
   alignSelf: "flex-start",
   height: 40,
   paddingHorizontal: 37,
-  borderRadius: 25
+  borderRadius: 25,
 }
 const BTN_GRADIENT_TEXT: TextStyle = {
   fontSize: 18,
   fontFamily: fonts.nunito.light,
-  color: "white"
+  color: "white",
 }
 const HERO_SEPARATOR: ViewStyle = {
   height: 1,
   width: 40,
   marginHorizontal: 20,
-  backgroundColor: "white"
+  backgroundColor: "white",
 }
 const HERO_PRICING: TextStyle = {
   fontSize: 14,
   fontFamily: fonts.nunito.light,
-  color: "white"
+  color: "white",
 }
 const HERO_IMG: ImageStyle = {
   width: 728,
   height: 626,
   maxWidth: "60%",
-  resizeMode: "contain"
+  resizeMode: "contain",
 }
