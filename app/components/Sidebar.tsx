d728b6e556daf7e500b349c2fda5d2b00eb551c5
elena-75 <contact.pro.elena@gmail.com>
2022-12-08 17:49:54 +0100

    component: Sidebar -> add admin sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,55 +1,76 @@
 import * as React from "react"
-import { StyleProp, TouchableOpacity, View, ViewStyle, Text, TextStyle, } from "react-native"
+import { StyleProp, TouchableOpacity, View, ViewStyle, Text, TextStyle } from "react-native"
 import { observer } from "mobx-react-lite"
 import { styling, palette } from "../theme"
-import { AntDesign, Entypo, Feather, Ionicons } from '@expo/vector-icons'
+import { AntDesign, Entypo, Feather, Ionicons } from "@expo/vector-icons"
 import { useNavigation } from "@react-navigation/native"
-
 
 export interface SidebarProps {
   style?: StyleProp<ViewStyle>
+  admin?: boolean
 }
 
 export const Sidebar = observer(function Sidebar(props: SidebarProps) {
-  const { style } = props
+  const { style, admin = false } = props
   const styles = [CONTAINER, style]
 
   const navigation = useNavigation()
   const goToOwnerHome = () => navigation.navigate("OwnerHome")
   const goToDao = () => navigation.navigate("Dao")
+  const goToAdminCreate = () => navigation.navigate("AdminCreate")
 
   return (
-    <View style={styles}>
-      <TouchableOpacity style={MENU_CONTAINER} onPress={goToOwnerHome}>
-        <AntDesign name="home" size={34} color="white" />
-        <Text style={MENU_TEXT}>Home</Text>
-      </TouchableOpacity>
+    <View style={[admin ? CONTAINER_ADMIN : CONTAINER_OWNER, styles]}>
+      {admin ? (
+        <View>
+          <TouchableOpacity style={MENU_CONTAINER} onPress={goToAdminCreate}>
+            <AntDesign name="form" size={34} color="white" />
+            <Text style={MENU_TEXT}>Créer</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity style={MENU_CONTAINER}>
-        <Entypo name="gauge" size={34} color="white" />
-        <Text style={MENU_TEXT}>Perf</Text>
-      </TouchableOpacity>
+          <TouchableOpacity style={MENU_CONTAINER} onPress={goToAdminCreate}>
+            <AntDesign name="form" size={34} color="white" />
+            <Text style={MENU_TEXT}>Vendre</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity style={MENU_CONTAINER}>
-        <View style={ICON_WRAPPER}>
-          <Feather name="heart" size={34} color="white" />
-          <View style={ICON_CONTAINER}>
-            <Feather name="activity" size={14} color="white" />
-          </View>
+          <TouchableOpacity style={MENU_CONTAINER} onPress={goToDao}>
+            <Feather name="users" size={34} color="white" />
+            <Text style={MENU_TEXT}>DAO</Text>
+          </TouchableOpacity>
         </View>
-        <Text style={MENU_TEXT}>Santé</Text>
-      </TouchableOpacity>
+      ) : (
+        <View>
+          <TouchableOpacity style={MENU_CONTAINER} onPress={goToOwnerHome}>
+            <AntDesign name="home" size={34} color="white" />
+            <Text style={MENU_TEXT}>Home</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity style={MENU_CONTAINER}>
-        <Ionicons name="briefcase-outline" size={34} color="white" />
-        <Text style={MENU_TEXT}>Invest</Text>
-      </TouchableOpacity>
+          <TouchableOpacity style={MENU_CONTAINER}>
+            <Entypo name="gauge" size={34} color="white" />
+            <Text style={MENU_TEXT}>Perf</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity style={MENU_CONTAINER} onPress={goToDao}>
-        <Feather name="users" size={34} color="white" />
-        <Text style={MENU_TEXT}>DAO</Text>
-      </TouchableOpacity>
+          <TouchableOpacity style={MENU_CONTAINER}>
+            <View style={ICON_WRAPPER}>
+              <Feather name="heart" size={34} color="white" />
+              <View style={ICON_CONTAINER}>
+                <Feather name="activity" size={14} color="white" />
+              </View>
+            </View>
+            <Text style={MENU_TEXT}>Santé</Text>
+          </TouchableOpacity>
 
+          <TouchableOpacity style={MENU_CONTAINER}>
+            <Ionicons name="briefcase-outline" size={34} color="white" />
+            <Text style={MENU_TEXT}>Invest</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={MENU_CONTAINER} onPress={goToDao}>
+            <Feather name="users" size={34} color="white" />
+            <Text style={MENU_TEXT}>DAO</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   )
 })
@@ -58,7 +79,14 @@ const CONTAINER: ViewStyle = {
   ...styling.CENTER_X,
   width: 100,
   paddingTop: 20,
-  backgroundColor: palette.pinkBright
+}
+
+const CONTAINER_OWNER: ViewStyle = {
+  backgroundColor: palette.pinkBright,
+}
+
+const CONTAINER_ADMIN: ViewStyle = {
+  backgroundColor: palette.blueDodger,
 }
 
 const MENU_CONTAINER: ViewStyle = {
@@ -69,7 +97,7 @@ const MENU_CONTAINER: ViewStyle = {
 
 const MENU_TEXT: TextStyle = {
   textAlign: "center",
-  color: "white"
+  color: "white",
 }
 
 const ICON_WRAPPER: ViewStyle = {
