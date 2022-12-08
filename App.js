e8e3ab74308e2dbee1ba34872ac60b76319bb967
Fernand Lime <fernand.lime@gmail.com>
2022-12-08 08:44:57 +0100

    disable wagmi

--- App.js
+++ App.js
@@ -7,13 +7,41 @@ import { registerRootComponent } from "expo"
 import * as SplashScreen from "expo-splash-screen"
 import { NativeBaseProvider } from "native-base"
 
+// import { configureChains, creatClient, WagmiConfig } from "wagmi"
+// import { mainnet, polygonMumbai } from "wagmi/chains"
+// import { infuraProvider } from "wagmi/providers/infura"
+
+// import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet"
+// import { InjectedConnector } from "wagmi/connectors/injected"
+// import { MetaMaskConnector } from "wagmi/connectors/metaMask"
+// import { WalletConnectConnector } from "wagmi/connectors/walletConnect"
+
 SplashScreen.preventAutoHideAsync()
+
+// const { chains, provider, webSocketProvider } = configureChains(
+//   [mainnet, polygonMumbai],
+//   [infuraProvider({ apiKey: process.env.INFURA_API_KEY })],
+// )
+
+// const wagmiClient = creatClient({
+//   autoConnect: true,
+//   connectors: [
+//     // new CoinbaseWalletConnector({ chains }),
+//     // new InjectedConnector({ chains, options: { name: "Injected" } }),
+//     new MetaMaskConnector({ chains }),
+//     // new WalletConnectConnector({ chains }),
+//   ],
+//   provider,
+//   webSocketProvider,
+// })
 
 function IgniteApp() {
   return (
+    // <WagmiConfig client={wagmiClient}>
     <NativeBaseProvider>
       <App hideSplashScreen={SplashScreen.hideAsync} />
     </NativeBaseProvider>
+    // </WagmiConfig>
   )
 }
 
