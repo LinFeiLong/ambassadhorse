39b94723615efd1973a3cd4454e43419130b949f
elena-75 <contact.pro.elena@gmail.com>
2022-12-10 06:58:16 +0100

    component: cardOnSale -> add card button + overlay
    add prop onPress + renamed end prop by deadline

--- app/components/CardOnSale.tsx
+++ app/components/CardOnSale.tsx
@@ -1,26 +1,49 @@
 import { observer } from 'mobx-react-lite'
 import * as React from 'react'
 import { Image, ImageStyle, StyleProp, Text, TextStyle, View, ViewStyle } from 'react-native'
-
-import { fonts, palette, styling } from '../theme'
+import { AntDesign } from '@expo/vector-icons'
+import { styling, fonts, gradients, palette, colors } from '../theme'
+import { Btn } from './Btn';
 
 export interface CardOnSaleProps {
   style?: StyleProp<ViewStyle>
-  end: string
+  deadline: string
   picture: any
   title: string
   price: number
   tokenPrice: number
+  onPress: () => void
 }
 
 export const CardOnSale = observer(function CardOnSale(props: CardOnSaleProps) {
-  const { style, end, picture, title, price, tokenPrice } = props
+  const { style, deadline, picture, title, price, tokenPrice, onPress } = props
   const styles = [CONTAINER, style]
 
   return (
     <View style={styles}>
-      <Text style={CAPTION}>{end}</Text>
-      <Image style={IMG} source={picture} />
+      {
+        deadline
+          ? (
+            <View style={CAPTION_CONTAINER}>
+              <AntDesign name="clockcircleo" size={24} color="white" />
+              <Text style={CAPTION}>{deadline}</Text>
+            </View>
+          )
+          : null
+      }
+
+      <View style={IMG_CONTAINER}>
+        <Image style={IMG} source={picture} />
+        <Btn
+          style={BTN_INFO}
+          text="En savoir +"
+          textStyle={BTN_TEXT}
+          gradient={gradients.default}
+          gradientStyle={BTN_GRADIENT}
+          onPress={() => onPress}
+        />
+      </View>
+
       <Text style={TITLE}>{title}</Text>
       <Text style={[SUBTITLE, { color: "#179cff" }]}>Total price {price}</Text>
       <Text style={[SUBTITLE, { color: "#ff1a92" }]}>Token start price {tokenPrice}</Text>
@@ -37,14 +60,27 @@ const CONTAINER: ViewStyle = {
   margin: 10,
 }
 
-const CAPTION: TextStyle = {
+const CAPTION_CONTAINER: ViewStyle = {
+  ...styling.ROW_CENTER_Y,
   position: "absolute",
   zIndex: 10,
-  top: 5,
-  left: 5,
+  top: 0,
+  left: 0,
+  right: 0,
+  padding: 10,
+  backgroundColor: colors.cardOverlay
+}
+
+const CAPTION: TextStyle = {
   fontFamily: fonts.nunito.bold,
   fontSize: 13,
-  color: "black",
+  paddingHorizontal: 5,
+  color: "white",
+}
+
+const IMG_CONTAINER: ViewStyle = {
+  position: "relative",
+  zIndex: 1,
 }
 
 const IMG: ImageStyle = {
@@ -53,6 +89,27 @@ const IMG: ImageStyle = {
   maxWidth: "100%",
   maxHeight: "100%",
   resizeMode: "cover",
+}
+
+const BTN_INFO: ViewStyle = {
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  height: 30,
+}
+
+const BTN_GRADIENT: ViewStyle = {
+  ...styling.CENTER_Y,
+  height: 30,
+  paddingHorizontal: 20,
+  borderRadius: 25,
+}
+
+const BTN_TEXT: TextStyle = {
+  fontFamily: fonts.nunito.light,
+  fontSize: 14,
+  paddingLeft: 5,
+  color: "black",
 }
 
 const TITLE: TextStyle = {
