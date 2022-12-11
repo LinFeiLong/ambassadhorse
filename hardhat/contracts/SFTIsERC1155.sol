a978af3947d8e288619addddc4108a195061f6cf
Fernand Lime <fernand.lime@gmail.com>
2022-12-11 04:11:42 +0100

    fix: import error

---
+++ hardhat/contracts/SFTIsERC1155.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract Horses is ERC1155, Ownable {
+
+}
