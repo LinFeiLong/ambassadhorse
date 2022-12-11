9dd65f6b5581045b16efd6ffbdf1807fbee79103
Fernand Lime <fernand.lime@gmail.com>
2022-12-11 04:14:43 +0100

    remove unused contracts

--- hardhat/contracts/Horses.sol
+++ hardhat/contracts/Horses.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Horses is ERC1155, Ownable {
 
