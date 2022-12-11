049f340e625b59e0bfc2a29ed08ffe97ffb03c24
Fernand Lime <fernand.lime@gmail.com>
2022-12-11 10:49:31 +0100

    add api keys

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,6 +2,10 @@ import '@nomicfoundation/hardhat-toolbox'
 
 import { HardhatUserConfig } from 'hardhat/config'
 
+require("dotenv").config()
+
+const { INFURA, ETHERSCAN_API_KEY, PRIVATE_KEY } = process.env
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.17",
@@ -18,7 +22,20 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       chainId: 80001,
+      from: "0x4ac8E6177D6bA59EC47087fb1fFD2F100eaFe949",
     },
+    mumbai: {
+      url: INFURA,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  gasReporter: {
+    currency: "EUR",
+    gasPrice: 21,
+    enabled: true,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 }
 
